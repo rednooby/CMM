c191cc9ce7e77f4f48d9587c863b93b99ad92dc4
jinheon <rednooby@gmail.com>
2017-05-09 16:54:11 +0900

    로그아웃버튼 링크 연결하기

--- main/views.py
+++ main/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 def index(request):
 	print(request.user, 'has login') #유저 로그에 남기기
 	return render(request, 'main/index.html')
+
+def login(request):
+	return render(request, 'login/login.html')
