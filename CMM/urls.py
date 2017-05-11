26895a368c50a9ae6d098c0d2843ac17780a6148
rednooby <rednooby@gmail.com>
2017-05-11 14:41:25 +0900

    로그아웃 완성

--- CMM/urls.py
+++ CMM/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/', include('login.urls')),
     url(r'^index/', include('main.urls')),
-    url(r'^index/', include('django.contrib.auth.urls')),
 ]
--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views #logout하기위해
 from . import views
+from django.conf import settings
 
 #/index/??
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^logout/', auth_views.logout, name='logout', kwargs={'next_page': 'login/login.html'}),
- 
+    url(r'^logout/', auth_views.logout, name='logout', 
+    	kwargs={'next_page': settings.LOGIN_URL}),
 ]
