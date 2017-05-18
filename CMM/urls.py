4cb9cd5c0b116d5b7beeffe991b8d6d832c84a8d
jinheon <rednooby@gmail.com>
2017-05-18 15:01:14 +0900

    회원정보수정 수정전 백업

--- CMM/urls.py
+++ CMM/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.shortcuts import redirect
 
+'''
+##최상위 주소##
+def root(request):
+	return redirect('login:login')
+'''
 urlpatterns = [
+	#url(r'^$', root, name='root'), ##최상위 주소##
+	#url(r'^$', lambda r: redirect('login:login'), name='root'), ##최상위 주소##
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('login.urls')),
     url(r'^index/', include('main.urls')),
--- login/forms.py
+++ login/forms.py
@@ -53,6 +53,36 @@ class UserCreationForm(forms.ModelForm): #모델폼 사용
             user.save()
         return user
 
+##패스워드 변경 폼##
+'''
+class PasswordChangeForm(SetPasswordForm):
+    """
+    A form that lets a user change their password by entering their old
+    password.
+    """
+    error_messages = dict(SetPasswordForm.error_messages, **{
+        'password_incorrect': _("Your old password was entered incorrectly. Please enter it again."),
+    })
+    old_password = forms.CharField(
+        label=_("Old password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autofocus': ''}),
+    )
+
+    field_order = ['old_password', 'new_password1', 'new_password2']
+
+    def clean_old_password(self):
+        """
+        Validates that the old_password field is correct.
+        """
+        old_password = self.cleaned_data["old_password"]
+        if not self.user.check_password(old_password):
+            raise forms.ValidationError(
+                self.error_messages['password_incorrect'],
+                code='password_incorrect',
+            )
+        return old_password
+'''
 
 
 
