46b99293dc6a781f3db57acf992e0ce9f9143136
jinheon <rednooby@gmail.com>
2017-06-24 17:31:40 +0900

    그래프 초안 완성

--- login/views.py
+++ login/views.py
@@ -29,14 +29,15 @@ def my_list(request, id): #ActList의 id
 	qs = ActList.objects.filter(act__email=request.user.email, id=id)
 	qs_info = BankBook.objects.filter(name_id=id).order_by('-act_date')
 	qs_graph = BankBook.objects.filter(name_id=id).order_by('act_date')
+	qs_income = BankBook.objects.filter(name_id=id, act_part="수입").order_by('act_date')#수입
+	qs_expenses = BankBook.objects.filter(name_id=id,act_part="지출").order_by('act_date')#지출
 	qs_li = ActList.objects.filter(act__email=request.user.email)
 	
 	qs_total = BankBook.objects.filter(name_id=id).aggregate(Sum('act_total'))
 
-	
+	print(qs_income)
 
-	print(qs_total)
-	return render(request, 'login/my_list.html', {'qs': qs, 'qs_info':qs_info, 'qs_li':qs_li, 'qs_total':qs_total, 'qs_graph':qs_graph})
+	return render(request, 'login/my_list.html', {'qs': qs, 'qs_info':qs_info, 'qs_li':qs_li, 'qs_total':qs_total, 'qs_income':qs_income, 'qs_expenses':qs_expenses, 'qs_graph':qs_graph})
 
 
 ##통장 사용정보 출력##	/index/my_view/id/$
